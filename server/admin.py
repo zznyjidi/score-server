379958a32693422d3a9e9ec14ce5911308d18b3d
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-12-22 13:09:36 -0500

    fix: match admin default env with server

--- server/admin.py
+++ server/admin.py
@@ -5,9 +5,9 @@ from database import PostgresDB
 async def prepare():
     global db
     db = await PostgresDB(
-        host=os.getenv("POSTGRES_HOST", "db"), 
+        host=os.getenv("POSTGRES_HOST", "127.0.0.1"), 
         port=os.getenv("POSTGRES_PORT", 5432), 
-        user=os.getenv("POSTGRES_USER", "postgres"),
-        password=os.getenv("POSTGRES_PASS", None), 
+        user=os.getenv("POSTGRES_USER", "score-server"),
+        password=os.getenv("POSTGRES_PASS", "password"), 
         database=os.getenv("POSTGRES_DB", "scores")
     )
