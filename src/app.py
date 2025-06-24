eb0489da5f65f8e641c19b15fc4fc51e5c423046
shift.0x7d8 <shift.0x7d8@ms.zzny.fun>
2025-06-23 23:42:07 -0400

    refactor: add init database to app context

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 from aiohttp import web
 
-from server import server
+from server import server, setup
 
 if __name__ == '__main__':
     app = web.Application()
+    app.cleanup_ctx.append(setup.init_database)
     app.add_routes(server.routes)
     web.run_app(app)
