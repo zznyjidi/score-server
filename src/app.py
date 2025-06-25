c66e2227a88549a279342ca2a8df7d3361761fbb
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-06-25 13:44:27 -0400

    feat: add valkey to deployment

--- src/app.py
+++ src/app.py
@@ -1,10 +1,16 @@
+import sys
 from aiohttp import web
 
 from server import server, setup
 
 if __name__ == '__main__':
     app = web.Application()
+
+    app[setup.config_key] = setup.parse_config(sys.argv)
+
     app.cleanup_ctx.append(setup.init_database)
     app.cleanup_ctx.append(setup.init_cache)
+
     app.add_routes(server.routes)
+
     web.run_app(app)
--- src/server/setup.py
+++ src/server/setup.py
@@ -1,4 +1,6 @@
 
+import argparse
+import os
 from aiohttp import web
 import redis.asyncio as aioredis
 
@@ -7,6 +9,26 @@ from .database import PostgresDB
 config_key = web.AppKey("config", dict[str, str | None])
 postgres_key = web.AppKey("postgres", PostgresDB)
 redis_key = web.AppKey("redis", aioredis.Redis)
+
+def parse_config(argv: list[str]) -> dict[str, str | None]:
+    parser = argparse.ArgumentParser(
+        prog='Score API Server', 
+        description="Server for Score Storage and Leaderboard. ", 
+        epilog=r'Github: https://github.com/zznyjidi/score-server'
+    )
+    parser.add_argument('--postgres', help='Connection URL for PostgreSQL', default=None)
+    parser.add_argument('--redis', help='Connection URL for Redis', default=None)
+    arg_config, _ = parser.parse_known_args(argv)
+
+    config: dict[str, str | None] = {
+        'postgres': arg_config.postgres, 
+        'redis': arg_config.redis
+    }
+
+    if config['redis'] is None:
+        config['redis'] = os.getenv('REDIS_URL')
+
+    return config
 
 async def init_database(app: web.Application):
     app[postgres_key] = await PostgresDB(dsn=app[config_key].get('postgres'))
